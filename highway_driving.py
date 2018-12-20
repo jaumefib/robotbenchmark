8aedf65a40ae003953fd397fff9e0f4c0af95455
Jaume <jaume.pladevall@est.fib.upc.edu>
2018-12-21 00:05:24 +0100

    v 0.2

--- highway_driving.py
+++ highway_driving.py
@@ -18,7 +18,7 @@ sensorsNames = [
     'left']
 sensors = {}
 
-maxSpeed = 80 #80
+maxSpeed = 80
 driver = Driver()
 driver.setSteeringAngle(0.0)  # go straight
 
@@ -26,16 +26,6 @@ driver.setSteeringAngle(0.0)  # go straight
 for name in sensorsNames:
     sensors[name] = driver.getDistanceSensor('distance sensor ' + name)
     sensors[name].enable(10)
-
-# get and enable the GPS
-gps = driver.getGPS('gps')
-gps.enable(10)
-
-# get the camera
-camera = driver.getCamera('camera')
-# uncomment those lines to enable the camera
-camera.enable(50)
-camera.recognitionEnable(50)
 
 # Range of sensors
 fR = sensors['front'].getMaxValue()
@@ -48,14 +38,13 @@ fL0R = sensors['front left 0'].getMaxValue()
 fL1R = sensors['front left 1'].getMaxValue()
 fL2R = sensors['front left 2'].getMaxValue()
 
-# Param de optimitzacio
-gamma1 = 0.2
-gamma2 = 0.1
-alpha1 = 0.2
-alpha2 = 0.2
+# Emmagatzema la distancia que ens hem apartat cap a l'esquerra
+# El valor de steering cap a l'esquerra es negatiu i cap a la dreta positiu
+dLeft = 0
 
 while driver.step() != -1:
-    # adjust the speed according to the value returned by the front distance sensor
+    
+    # Distance value of sensors
     fD = sensors['front'].getValue()
     lD = sensors['left'].getValue()
     rD = sensors['right'].getValue()
@@ -65,22 +54,22 @@ while driver.step() != -1:
     fL0D = sensors['front left 0'].getValue()
     fL1D = sensors['front left 1'].getValue()
     fL2D = sensors['front left 2'].getValue()
-    
     fLeft = fL0D/fL0R + fL1D/fL1R + fL2D/fL2R
     fRight = fR0D/fR0R + fR1D/fR1R + fR2D/fR2R 
     
-    speed = min(maxSpeed * fD / fR, maxSpeed * (fLeft+fRight))
-    driver.setCruisingSpeed(min(speed, 200))
-    
-    # brake if we need to reduce the speed because there is a car in front
+    speed = maxSpeed * fD / fR
+    driver.setCruisingSpeed(speed)
+    # brake if we need to reduce the speed
     speedDiff = driver.getCurrentSpeed() - speed
-    # steer left if there is no car in the left
-    steer = (fD/fR - lD/lR - fLeft*alpha1)*gamma1 + (-fD/fR + rD/rR + fRight*alpha2)*gamma2
-    print(lD/fR, fLeft, fD/lR, fRight, rD/rR)
+    driver.setBrakeIntensity(max(min(speedDiff / speed, 1), 0))
+    
+    steer =  0
+    steer = min(max(steer, -1), 1)
+    dLeft = dLeft + steer
+    driver.setSteeringAngle(steer)
+    
+    #print(lD/lR, fLeft, fD/fR, fRight, rD/rR)
+    print(lD/lR)
     print(steer)
-    print(speed)
+    #print(speed)
     
-    speed = max(min(speedDiff/speed,1), 0)
-    
-    driver.setBrakeIntensity(speed)
-    driver.setSteeringAngle(steer) # si steer positiu gir a ma dreta, si negatiu gir a ma esquerra
